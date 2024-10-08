0b2925f10ade7f71d537cc7ca8c19770b929b9bb
Gianni <giannirosato@proton.me>
2024-10-08 00:59:18 -0400

    Introduce noise normalization strength

--- Source/Lib/Globals/enc_settings.c
+++ Source/Lib/Globals/enc_settings.c
@@ -908,6 +908,10 @@ EbErrorType svt_av1_verify_settings(SequenceControlSet* scs) {
     if (SVT_EFFECTIVE_BIT_DEPTH(config->encoder_bit_depth) == 8 && (config->hbd_mds == 1 || config->hbd_mds == 2)) {
         SVT_WARN("Please use 10-bit encoding if you want to take advantage of hbd-mds 1 and 2.\n");
         SVT_ERROR("Full high bit depth and hybrid 8/10 mode decision are not supported when encoder bit depth is 8\n");
+    }
+
+    if (config->noise_norm_strength > 4) {
+        SVT_ERROR("Noise normalization strength must be between 0 and 4\n");
         return_error = EB_ErrorBadParameter;
     }
 
@@ -1082,7 +1086,7 @@ EbErrorType svt_av1_set_default_params(EbSvtAv1EncConfiguration* config_ptr) {
     // and no extra ref-buffer memory allocated.
     config_ptr->max_managed_refs = 0;
 
-
+    config_ptr->noise_norm_strength               = 1;
     return return_error;
 }
 
@@ -1249,6 +1253,10 @@ void svt_av1_print_lib_params(SequenceControlSet* scs) {
 
         if (config->hbd_mds != DEFAULT) {
             SVT_INFO("SVT [config]: High Bit Depth Mode Decision setting \t\t\t\t\t: %d\n", config->hbd_mds);
+        }
+
+        if (config->noise_norm_strength > 0) {
+            SVT_INFO("SVT [config]: Noise Normalization Strength \t\t\t\t\t: %d\n", config->noise_norm_strength);
         }
     }
 #if DEBUG_BUFFERS
@@ -2322,6 +2330,7 @@ EB_API EbErrorType svt_av1_enc_parse_parameter(EbSvtAv1EncConfiguration* config_
         {"enable-tf", &config_struct->enable_tf},
         {"tf-strength", &config_struct->tf_strength},
         {"max-tx-size", &config_struct->max_tx_size},
+        {"noise-norm-strength", &config_struct->noise_norm_strength},
     };
 
     const size_t uint8_opts_size = sizeof(uint8_opts) / sizeof(uint8_opts[0]);
